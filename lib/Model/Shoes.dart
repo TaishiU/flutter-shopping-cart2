64c198a2eac867192fc26b83dbd9449c9e0ff2ea
TaishiU <nbadreamteam3@gmail.com>
2021-09-28 11:17:36 +0900

    モデル作成

---
+++ lib/Model/Shoes.dart
@@ -0,0 +1,30 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Shoes {
+  String id;
+  String name;
+  String description;
+  int price;
+  Map<String, String> images;
+  Map<String, String> size;
+
+  Shoes({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.price,
+    required this.images,
+    required this.size,
+  });
+
+  factory Shoes.fromDoc(DocumentSnapshot doc) {
+    return Shoes(
+      id: doc['id'],
+      name: doc['name'],
+      description: doc['description'],
+      price: doc['price'],
+      images: Map<String, String>.from(doc['images']),
+      size: Map<String, String>.from(doc['size']),
+    );
+  }
+}
