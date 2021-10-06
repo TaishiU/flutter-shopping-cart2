91dd3db2ebaad709f216a12206c601b4928d2f70
TaishiU <nbadreamteam3@gmail.com>
2021-10-06 11:36:13 +0900

    README.md

--- lib/Screens/CartScreen.dart
+++ lib/Screens/CartScreen.dart
@@ -77,179 +77,209 @@ class _CartScreenState extends State<CartScreen> {
           ),
         ],
       ),
-      body: SingleChildScrollView(
-        // physics: BouncingScrollPhysics(
-        //   parent: AlwaysScrollableScrollPhysics(),
-        // ),
-        child: StreamBuilder(
-          stream: cartRef
-              .doc(widget.currentUserId)
-              .collection('cartShoes')
-              .orderBy('timestamp', descending: true)
-              .snapshots(),
-          builder:
-              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-            if (!snapshot.hasData) {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            List<DocumentSnapshot> favoriteShoesList = snapshot.data!.docs;
-            for (var shoes in favoriteShoesList) {
-              subTotalPrice += shoes['price'] as int;
-              final formatter = NumberFormat('#,###');
-              subTotalResult = formatter.format(subTotalPrice);
-              // if (subTotalPrice < 15000) {
-              //   /*小計が15,000円未満であれば配送手数料550円がかかる*/
-              //   tax = 550;
-              //   totalPrice = subTotalPrice + tax;
-              // } else {
-              //   /*小計が15,000円以上であれば配送手数料円がかかる*/
-              //   tax = 0;
-              //   totalPrice = subTotalPrice + tax;
-              // }
-            }
-            return Column(
-              children: [
-                Container(
-                  height: 90,
-                  color: Colors.yellow,
-                  child: Center(
-                    child: Text(
-                      '${favoriteShoesList.length.toString()} 商品  |  ¥$subTotalPrice',
-                      style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold,
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: StreamBuilder(
+              stream: cartRef
+                  .doc(widget.currentUserId)
+                  .collection('cartShoes')
+                  .orderBy('timestamp', descending: true)
+                  .snapshots(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                List<DocumentSnapshot> favoriteShoesList = snapshot.data!.docs;
+                for (var shoes in favoriteShoesList) {
+                  subTotalPrice += shoes['price'] as int;
+                  final formatter = NumberFormat('#,###');
+                  subTotalResult = formatter.format(subTotalPrice);
+                  // if (subTotalPrice < 15000) {
+                  //   /*小計が15,000円未満であれば配送手数料550円がかかる*/
+                  //   tax = 550;
+                  //   totalPrice = subTotalPrice + tax;
+                  // } else {
+                  //   /*小計が15,000円以上であれば配送手数料円がかかる*/
+                  //   tax = 0;
+                  //   totalPrice = subTotalPrice + tax;
+                  // }
+                }
+                return Column(
+                  children: [
+                    Container(
+                      height: 90,
+                      color: Colors.white,
+                      child: Center(
+                        child: Text(
+                          '${favoriteShoesList.length.toString()} 商品  |  ¥$subTotalResult',
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
                       ),
                     ),
-                  ),
-                ),
-                ListView(
-                  shrinkWrap: true,
-                  physics: NeverScrollableScrollPhysics(),
-                  children: favoriteShoesList.map((favoriteShoes) {
-                    FavoriteAndCart cart =
-                        FavoriteAndCart.fromDoc(favoriteShoes);
+                    ListView(
+                      shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
+                      children: favoriteShoesList.map((favoriteShoes) {
+                        FavoriteAndCart cart =
+                            FavoriteAndCart.fromDoc(favoriteShoes);
 
-                    /*シューズの価格をintからStringに置き換え*/
-                    final formatter = NumberFormat("#,###");
-                    var shoesPrice = formatter.format(cart.price);
+                        /*シューズの価格をintからStringに置き換え*/
+                        final formatter = NumberFormat("#,###");
+                        var shoesPrice = formatter.format(cart.price);
 
-                    return Padding(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                      child: Column(
-                        children: [
-                          GestureDetector(
-                            onTap: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => ProductScreen(
-                                    currentUserId: widget.currentUserId,
-                                    shoesId: cart.shoesId,
-                                    shoesPrice: shoesPrice,
-                                  ),
-                                ),
-                              );
-                            },
-                            child: Container(
-                              color: Colors.transparent,
-                              child: Row(
-                                children: [
-                                  Container(
-                                    height: 100,
-                                    width: 100,
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(20),
-                                      image: DecorationImage(
-                                        image: NetworkImage(cart.image),
-                                        fit: BoxFit.cover,
+                        return Padding(
+                          padding:
+                              EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                          child: Column(
+                            children: [
+                              GestureDetector(
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => ProductScreen(
+                                        currentUserId: widget.currentUserId,
+                                        shoesId: cart.shoesId,
+                                        shoesPrice: shoesPrice,
                                       ),
                                     ),
-                                  ),
-                                  SizedBox(width: 10),
-                                  Expanded(
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.spaceBetween,
+                                  );
+                                },
+                                child: Container(
+                                  color: Colors.transparent,
+                                  child: Row(
+                                    children: [
+                                      Container(
+                                        height: 100,
+                                        width: 100,
+                                        decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(20),
+                                          image: DecorationImage(
+                                            image: NetworkImage(cart.image),
+                                            fit: BoxFit.cover,
+                                          ),
+                                        ),
+                                      ),
+                                      SizedBox(width: 10),
+                                      Expanded(
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
                                           children: [
-                                            Container(
-                                              width: MediaQuery.of(context)
-                                                      .size
-                                                      .width *
-                                                  0.5,
-                                              child: Text(
-                                                cart.name,
-                                                style: TextStyle(
-                                                  fontSize: 15,
-                                                  fontWeight: FontWeight.bold,
+                                            Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment
+                                                      .spaceBetween,
+                                              children: [
+                                                Container(
+                                                  width: MediaQuery.of(context)
+                                                          .size
+                                                          .width *
+                                                      0.5,
+                                                  child: Text(
+                                                    cart.name,
+                                                    style: TextStyle(
+                                                      fontSize: 15,
+                                                      fontWeight:
+                                                          FontWeight.bold,
+                                                    ),
+                                                  ),
                                                 ),
-                                              ),
+                                                GestureDetector(
+                                                  onTap: () {
+                                                    Firestore().deleteFromCart(
+                                                      currentUserId:
+                                                          widget.currentUserId,
+                                                      shoesId: cart.shoesId,
+                                                    );
+                                                    calculatePrice(
+                                                        shoesPrice: cart.price);
+                                                  },
+                                                  child: Icon(
+                                                    Icons.clear,
+                                                    color: Colors.grey,
+                                                  ),
+                                                ),
+                                              ],
                                             ),
-                                            GestureDetector(
-                                              onTap: () {
-                                                Firestore().deleteFromCart(
-                                                  currentUserId:
-                                                      widget.currentUserId,
-                                                  shoesId: cart.shoesId,
-                                                );
-                                                calculatePrice(
-                                                    shoesPrice: cart.price);
-                                              },
-                                              child: Icon(
-                                                Icons.clear,
+                                            Text(
+                                              cart.type,
+                                              style: TextStyle(
                                                 color: Colors.grey,
                                               ),
                                             ),
-                                          ],
-                                        ),
-                                        Text(
-                                          cart.type,
-                                          style: TextStyle(
-                                            color: Colors.grey,
-                                          ),
-                                        ),
-                                        SizedBox(height: 10),
-                                        Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.spaceBetween,
-                                          children: [
-                                            Text(
-                                              'サイズ  ${cart.size}',
-                                              style: TextStyle(
-                                                  //color: Colors.grey,
+                                            SizedBox(height: 10),
+                                            Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment
+                                                      .spaceBetween,
+                                              children: [
+                                                Text(
+                                                  'サイズ  ${cart.size}',
+                                                  style: TextStyle(
+                                                      //color: Colors.grey,
+                                                      ),
+                                                ),
+                                                Text(
+                                                  '¥$shoesPrice',
+                                                  style: TextStyle(
+                                                    fontSize: 15,
+                                                    fontWeight: FontWeight.bold,
                                                   ),
-                                            ),
-                                            Text(
-                                              '¥$shoesPrice',
-                                              style: TextStyle(
-                                                fontSize: 15,
-                                                fontWeight: FontWeight.bold,
-                                              ),
+                                                ),
+                                              ],
                                             ),
                                           ],
                                         ),
-                                      ],
-                                    ),
+                                      ),
+                                    ],
                                   ),
-                                ],
+                                ),
                               ),
-                            ),
+                            ],
                           ),
-                        ],
-                      ),
-                    );
-                  }).toList(),
+                        );
+                      }).toList(),
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              margin: EdgeInsets.only(bottom: 20, right: 20, left: 20),
+              height: 70,
+              width: MediaQuery.of(context).size.width,
+              child: ElevatedButton(
+                child: Text(
+                  'ご購入の手続きに進む',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ],
-            );
-          },
-        ),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.black,
+                  onPrimary: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+                onPressed: () {},
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
